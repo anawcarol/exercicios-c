7aa66dced45ed120f529b3f64c67f0331a5492e7
Ana Carolina <anacarolinamfialho@gmail.com>
2023-09-20 11:27:36 -0300

    código do primeiro exercicio

---
+++ log.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int lg(int N) {
+   int i, n;
+   i = 0;
+   n = 1;
+   while (n <= N/2) {
+      n = 2 * n;
+      i += 1;
+   }
+   return i;    
+}
+
+int main() {
+   int N = 1000; // Exemplo de valor para N
+   int result = lg(N); // Chamando a função lg
+   printf("O logaritmo na base 2 de %d é %d\n", N, result);
+   return 0;
+}
+
+
+
+
